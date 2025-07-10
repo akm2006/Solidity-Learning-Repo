02d1720b2d9930f1ebcdfa3f85550fe1ea0e10e7
AKM777 <aakashmandal10@gmail.com>
2025-07-10 17:05:18 +0530

    2nd

---
+++ ArrayStorage.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier: MIT 
+pragma solidity 0.8.19;
+
+contract SimpleStorage {
+    uint256 favoriteNumber ;
+
+    struct Person {
+
+        uint256 favoriteNumber;
+        string name;
+
+    }
+
+    Person[] public listOfPeople;
+    // Person public aak = Person({favoriteNumber:0 , name: "Aakash"});
+
+
+    function save(uint256 _favoriteNumber) public
+     {
+        favoriteNumber = _favoriteNumber ;
+        // favoriteNumber = _favoriteNumber + 1;
+
+    }
+    
+    function retreive() public view returns(uint256){
+        return favoriteNumber;
+
+    }
+
+    function addPerson(string memory name, uint256 _favoriteNumber) public{
+        listOfPeople.push(Person(_favoriteNumber, name));
+    }
+   
+}
