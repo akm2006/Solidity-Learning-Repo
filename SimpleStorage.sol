3dc2e18c6c99ca3f8e1a471eea9ef2f1bf84f1dc
AKM777 <aakashmandal10@gmail.com>
2025-07-11 11:08:11 +0530

    virtual&override

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,10 +1,22 @@
 //SPDX-License-Identifier: MIT 
-pragma solidity 0.8.19;
+pragma solidity 0.8.24;
 
 contract SimpleStorage {
     uint256 favoriteNumber ;
 
-    function save(uint256 _favoriteNumber) public
+    struct Person {
+
+        uint256 favoriteNumber;
+        string name;
+
+    }
+
+    Person[] public listOfPeople;
+    // Person public aak = Person({favoriteNumber:0 , name: "Aakash"});
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public virtual 
      {
         favoriteNumber = _favoriteNumber ;
         // favoriteNumber = _favoriteNumber + 1;
@@ -14,6 +26,11 @@ contract SimpleStorage {
     function retreive() public view returns(uint256){
         return favoriteNumber;
 
+    }
+
+    function addPerson(string memory name, uint256 _favoriteNumber) public{
+        listOfPeople.push(Person(_favoriteNumber, name));
+        nameToFavoriteNumber[name] = _favoriteNumber;
     }
    
 }
